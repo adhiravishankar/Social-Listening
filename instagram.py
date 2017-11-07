63ca1b90fe3493ef160170952d07253f24124039
Adhi Ravishankar <adhiravishankar@gmail.com>
2017-11-07 10:50:38 -0500

    add argparser

--- instagram.py
+++ instagram.py
@@ -2,6 +2,23 @@ import os
 from InstagramAPI import InstagramAPI
 
 
+def query_all_posts(tag, content_collection):
+    instagram_api = login()
+    tags = get_tags(instagram_api, tag)
+    for tag in tags:
+        media = get_media(instagram_api, tag['name'])
+        while True:
+            contents = []
+            if media['ranked_items']:
+                for item in media['ranked_items']:
+                    contents.append(put_item(item, instagram_api, content_collection))
+            if media['items']:
+                for item in media['items']:
+                    contents.append(put_item(item, instagram_api, content_collection))
+            if media['more_available']:
+                media = get_next_media(instagram_api, tag['name'], media['next_max_id'])
+
+
 def login():
     api = InstagramAPI(os.environ.get("INSTAGRAM_USERNAME"), os.environ.get("INSTAGRAM_PASSWORD"))
     api.login() # login
@@ -19,12 +36,18 @@ def get_media(instagram_api, tag):
     return instagram_api.LastJson
 
 
-def get_comments(instagram_api, media_id):
-    instagram_api.getMediaComments(media_id)
+def get_next_media(instagram_api, tag, next_max_id):
+    instagram_api.getHashtagFeed(tag, next_max_id)
     return instagram_api.LastJson
 
 
-def put_item(item):
+def get_comments(instagram_api, media_id, next_max_id=''):
+    instagram_api.getMediaComments(media_id, next_max_id)
+    return instagram_api.LastJson
+
+
+def put_item(item, instagram_api, content_collection):
+    contents = []
     content ={'network':'instagram',
               'content_type': 'post',
               'text': item['caption']['text'],
@@ -33,13 +56,29 @@ def put_item(item):
               'user_id': item['caption']['user_id'],
               'comment_count': item['comment_count'],
               'favourites_count': item['like_count']}
-    return content
+    if content_collection.count({'id': item['id'], 'network': 'instagram', 'content_type': 'post'}) == 0:
+        contents.append(content)
+    if item['comment_count'] > 0:
+        comments = get_comments(instagram_api, item['id'])
+        while True:
+            for comment in comments['comments']:
+                if content_collection.count({'id': comment['pk'], 'network': 'instagram', 'content_type': 'comment'}) == 0:
+                    contents.append(put_comment(comment, item['id']))
+            if comments['has_more_comments']:
+                comments = get_comments(instagram_api, item['id'], comments['next_max_id'])
+            else:
+                break
+    content_collection.insert_many(contents)
 
 
-def put_comment(item):
+def put_comment(item, item_id):
     content = {"network": 'instagram',
                "content_type": 'comment',
                "text": item['text'],
-               "created_at": item['created_time'],
-               "id": item['id'] }
+               "created_at": item['created_at'],
+               "id": item['pk'],
+               "media_id": item_id,
+               'user_id': item['user']['pk']}
+    if 'comment_like_count' in item:
+        content['favourites_count'] = item['comment_like_count']
     return content
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
+import argparse
 import os
 import dotenv
 from pymongo import MongoClient
 
+import instagram
 import mongodb
 import twitter2
 
 
-# get query
-query = "xbox one x"
-
 dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'), verbose=True)
 client = MongoClient(os.environ.get('DB_HOST'), 27017)
 db = client['sociallistening']
+
+parser = argparse.ArgumentParser()
+parser.add_argument('query')
+parser.add_argument('--stage')
+
+args = parser.parse_args()
+query = args.query
+stage = args.stage
 
 query = query.lower()
 tag = query.replace(' ', '')
 search = mongodb.put_search(query, db)
 content_collection = db['content']
 
-all_tweets = twitter2.query_all_tweets(query, content_collection, search)
+if stage is 'i':
+    instagram.query_all_posts(tag, content_collection)
+else:
+    twitter2.query_all_tweets(query, content_collection, search)
+    instagram.query_all_posts(tag, content_collection)
