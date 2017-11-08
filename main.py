3950e659862b80a850d0c56376c3da3b251934a0
Adhi Ravishankar <adhiravishankar@gmail.com>
2017-11-08 15:24:26 -0500

    start new stages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import dotenv
+import nltk
 from pymongo import MongoClient
 
 import instagram
@@ -25,8 +26,19 @@ tag = query.replace(' ', '')
 search = mongodb.put_search(query, db)
 content_collection = db['content']
 
-if stage is 'i':
+if stage is 'p':
+    search_content = content_collection.find({'search_id': search['_id'], 'processed_text': None}, {'_id': 1, 'text': 1})
+    for search_item in search_content:
+        text = search_item['text']
+        words = nltk.word_tokenize(text)
+        processed_words = []
+        for word in words:
+            if word not in nltk.corpus.stopwords.words('english'):
+                processed_words.append(word)
+elif stage is 'i':
     instagram.query_all_posts(tag, content_collection)
+elif stage is 't':
+    twitter2.query_all_tweets(query, content_collection, search)
 else:
     twitter2.query_all_tweets(query, content_collection, search)
     instagram.query_all_posts(tag, content_collection)
